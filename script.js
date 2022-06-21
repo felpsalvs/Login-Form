fa488d5c026be882e638bb9b09e83051322a447d
Felipe Alves <profelipealves@outlook.com>
2022-06-21 14:43:25 -0300

    adicionando menu lateral

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.getElementById('menu-area').style.display('0px');
+function menuToggle() {
+    let menuArea = document.getElementById('menu-area');
+
+    if(menuArea.style.width == '200px') {
+        menuArea.style.width = '0px';
+    } else {
+        menuArea.style.width = '200px';
+    }
+}
