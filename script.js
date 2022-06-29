ade0430d3fe00e4fbd0f4e49f58ab8a62b4c5705
Felipe Alves <profelipealves@outlook.com>
2022-06-29 10:16:31 -0300

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-function menuToggle() {
-    let menuArea = document.getElementById('menu-area');
-
-    if(menuArea.style.width == '200px') {
-        menuArea.style.width = '0px';
-    } else {
-        menuArea.style.width = '200px';
-    }
-}
