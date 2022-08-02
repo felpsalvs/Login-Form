678537aa42639d8b762cf01b1c4605dff7d598c2
Felipe Alves <profelipealves@outlook.com>
2022-08-02 16:00:29 -0300

    formata codigo

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 let validator = {
-    handleSubmit:(event)=>{
-        event.preventDefault();
-        let send = true;
+  handleSubmit: (event) => {
+    event.preventDefault();
+    let send = true;
 
-        let inputs = form.querySelectorAll('input');
+    let inputs = form.querySelectorAll("input");
 
-        for(let i = 0; i < inputs.length; i++){
-            let input = inputs[i];
-            let check = validator.checkInput(input);
-            if(check !== true){
-                send = false;
-                validator.showError(input, check);
-            }
-        }
-        
-        if(send){
-            form.submit();
-        }
-    },
-    checkInput:(input)=>{
-        let rules = input.getAttribute('data-rules');
-        if(rules !== null){
-            rules = rules.split('|');
-            for(let k in rules){
-                let rDetails = rules[k].split('=');
-                switch(rDetails[0]){
-                    case 'required':
-                        if(input.value == ''){
-                            return 'Campo necessário';
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      let check = validator.checkInput(input);
+      if (check !== true) {
+        send = false;
+        validator.showError(input, check);
+      }
+    }
+
+    if (send) {
+      form.submit();
+    }
+  },
+  checkInput: (input) => {
+    let rules = input.getAttribute("data-rules");
+    if (rules !== null) {
+      rules = rules.split("|");
+      for (let k in rules) {
+        let rDetails = rules[k].split("=");
+        switch (rDetails[0]) {
+          case "required":
+            if (input.value == "") {
+              return "Campo necessário";
             }
             break;
-            case 'min':
+          case "min":
             break;
-            }
         }
-}
+      }
+    }
+    return true;
+  },
+  showError: (input, error) => {
+    input.style.borderColor = "red";
+    let errorElement = document.createElement("div");
+    errorElement.classList.add("error");
+    errorElement.innerHTML = error;
 
-let form = document.querySelector('#email');
-form.addEventListener('submit', validator.handleSubmit);
+    input.parentElement.insetBefore(errorElement, input.ElementSibling);
+  },
+};
+
+let form = document.querySelector("#email");
+form.addEventListener("submit", validator.handleSubmit);
